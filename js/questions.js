741293e97b6fd9b60d4fccd84e424a9b1fa751b0
Wafa Bel Hadj Ammar <wafa.belhadjammar@esprit.tn>
2021-05-18 12:42:20 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qu’évoque le terme industrie 4.0 ?",
+    answer: "Une nouvelle révolution industrielle",
+    options: [
+      "Un groupement de quatre entreprises du même domaine industriel",
+      "Une chaine de production connectée en 4G",
+      "Une nouvelle révolution industrielle",
+      "Une quatriéme révolution des applications web"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quelles sont les 3 premières révolutions industrielles ?",
+    answer: "Les cités industrielles,production en série,diversifiée",
+    options: [
+      "Les peintures préhistoriques et l’impressionnisme",
+      "Les cités industrielles,production enSérie,diversifiée",
+      "Le transport par calèches,les autobus,la téléportation",
+      "Opérateur de fabrication,Roboticien,sécurité "
+    ]
+  },
+    {
+    numb: 3,
+    question: "Sur quelle base l’industrie 4.0 est-elle fondée ?",
+    answer: "Des systèmes intelligents autonomes",
+    options: [
+      "Des systèmes intelligents autonomes",
+      "Une production expatriée",
+      "La fabrication en masse d’un seul et même produit",
+      "Le besoin clients"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qu’est-ce que la fabrication additive ?",
+    answer: "L’assemblage de deux produits par un robot",
+    options: [
+      "L’union entre l’Homme et la Machine",
+      "La production par ajout de matières ",
+      "L’assemblage de deux produits par un robot",
+      "La production par lot"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qu’est-ce que la cobotique ?",
+    answer: "Une technique de fabrication alliant robots assistants et humains",
+    options: [
+      "Une chorégraphie interprétée par des drones",
+      "Une technique de fabrication alliant robots assistants et humains",
+      "Un thinktank sur le rôle des robots dans l’industrie",
+      "La collaboration robot-robot"
+    ]
+  }
+  
+];
---
+++ js/questions3.js
@@ -0,0 +1,60 @@
+﻿// creating an array and passing the number, questions, options, and answers
+
+let questions3 = [
+    {
+    numb: 1,
+    question: "Question 1 niveau 3?",
+    answer: "Une nouvelle révolution industrielle",
+    options: [
+      "Un groupement de quatre entreprises du même domaine industriel",
+      "Une chaine de production connectée en 4G",
+      "Une nouvelle révolution industrielle",
+      "Une quatriéme révolution des applications web"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Question 2 niveau 3?",
+    answer: "Les cités industrielles,production en série,diversifiée",
+    options: [
+      "Les peintures préhistoriques et l’impressionnisme",
+      "Les cités industrielles,production en série,diversifiée",
+      "Le transport par calèches, les autobus, la téléportation",
+      "Opérateur de fabrication,Roboticien,sécurité "
+    ]
+  },
+    {
+    numb: 3,
+    question: "Question 3 niveau 3 ?",
+    answer: "Des systèmes intelligents autonomes",
+    options: [
+      "Des systèmes intelligents autonomes",
+      "Une production expatriée",
+      "La fabrication en masse d’un seul et même produit",
+      "Le besoin clients"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Question 4 niveau 3 ?",
+    answer: "L’assemblage de deux produits par un robot",
+    options: [
+      "L’union entre l’Homme et la Machine",
+      "La production par ajout de matières ",
+      "L’assemblage de deux produits par un robot",
+      "La production par lot"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Question 5 niveau 3 ?",
+    answer: "Une technique de fabrication alliant robots assistants et humains",
+    options: [
+      "Une chorégraphie interprétée par des drones",
+      "Une technique de fabrication alliant robots assistants et humains",
+      "Un thinktank sur le rôle des robots dans l’industrie",
+      "La collaboration robot-robot"
+    ]
+  }
+  
+];
